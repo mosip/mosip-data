fb1616f4a1e5dc8c08ff51e3989821961b12bf75
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2026-06-10 12:07:46 +0530

    Update data-uploader.py
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- mosip_master/data_upgrade/1.2.0.1_to_1.3.0/data-uploader.py
+++ mosip_master/data_upgrade/1.2.0.1_to_1.3.0/data-uploader.py
@@ -12,6 +12,7 @@ import json
 import sys
 import time
 import psycopg2
+from psycopg2 import sql
 import openpyxl
 
 parser = argparse.ArgumentParser(description='This is CSV/xlsx file uploader script.invokes 1.2.0.1 bulk upload endpoints')
@@ -48,7 +49,10 @@ def get_seed_value():
   seed_value = None
   conn = psycopg2.connect(database="mosip_master", user = args.dbusername, password = args.dbpassword, host = args.dbhost, port = args.dbport)
   cursor = conn.cursor()
-  cursor.execute("select id from master."+args.table+" order by id desc limit 20")
+  cursor.execute(
+      sql.SQL("select id from master.{} order by id desc limit 20")
+      .format(sql.Identifier(args.table))
+  )
   for row in cursor.fetchall():
     id_value = row[0]
     if id_value is None:
